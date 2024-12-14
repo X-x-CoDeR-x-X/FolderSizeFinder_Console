b96887d7bb67bb2a2aa13647ede234142ec4ada0
X-x-CoDeR-x-X <xocderx@gmail.com>
2024-12-15 00:05:58 +0200

    Dev

--- src/FolderSizeCalculator.java
+++ src/FolderSizeCalculator.java
@@ -1,32 +1,37 @@
 import java.io.File;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class FolderSizeCalculator extends RecursiveTask<Long> {
-    private File folder;
+    private Node node;
 
-    public FolderSizeCalculator(File folder) {
-        this.folder = folder;
+    public FolderSizeCalculator(Node node) {
+        this.node = node;
     }
 
     @Override
     protected Long compute() {
+        File folder = node.getFolder();
         if (folder.isFile()) {
+            long length = folder.length();
+            node.setSize(length);
             return folder.length();
         }
         long sum = 0;
         List<FolderSizeCalculator> subTasks = new LinkedList<>();
         File[] files = folder.listFiles();
         for (File file : files) {
-            FolderSizeCalculator task = new FolderSizeCalculator(file);
+            Node child = new Node(file);
+            FolderSizeCalculator task = new FolderSizeCalculator(child);
             task.fork();
             subTasks.add(task);
+            node.addChild(child);
         }
         for (FolderSizeCalculator task : subTasks) {
             sum += task.join();
         }
+        node.setSize(sum);
         return sum;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,20 @@
 import java.io.File;
-import java.util.Scanner;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
-    private static String[] sizeNames =
-            {"b", "kb", "Mb", "Gb"};
-
-
     public static void main(String[] args) {
 
-        String folderPath = "d:/coding/";
+        String folderPath = "D:/10tons";
         File file = new File(folderPath);
-        Long startTime = System.currentTimeMillis();
-        System.out.println(getHumanReadableSize(getFolderSize(file)));
-        long endTime = System.currentTimeMillis();
-        System.out.println("Time taken: " + (endTime - startTime) + "ms");
+        Node root = new Node(file);
 
-//        Long startTime = System.currentTimeMillis();
-//        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
-//        ForkJoinPool pool = new ForkJoinPool();
-//        long size = pool.invoke(calculator);
-//        System.out.println(size);
-//        long endTime = System.currentTimeMillis();
-//        System.out.println("Time taken: " + (endTime - startTime) + "ms");
+        long fstartTime = System.currentTimeMillis();
+        FolderSizeCalculator calculator = new FolderSizeCalculator(root);
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.invoke(calculator);
+        System.out.println(root);
 
-
-//        while (true) {
-//            System.out.println("Введите путь к папке или файлу:");
-//            String path = new Scanner(System.in).nextLine();
-//            File file = new File(path);
-//            getFolderSize(file);
-//        }
-    }
-
-    public static Long getFolderSize(File path) {
-        if (path.isFile()) {
-            return path.length();
-        }
-        Long dirSize = 0L;
-        File[] files = path.listFiles();
-        for (File file : files) {
-            dirSize += getFolderSize(file);
-        }
-        return dirSize;
-    }
-
-    public static String getHumanReadableSize(long length) {
-        int power = (int) (Math.log(length) / Math.log(1024));
-        double value = length / Math.pow(1024, power);
-        double roundedValue = Math.round(value * 100) / 100.;
-        return roundedValue + " " + sizeNames[power];
+        long fendTime = System.currentTimeMillis();
+        System.out.println("ForkJoin time taken: " + (fendTime - fstartTime) + "ms");
     }
 }
